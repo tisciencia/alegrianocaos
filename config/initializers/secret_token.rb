6269e646eb74cd53f88455cd9d6d8b82c2ab920c
Tiago Sciencia <tisciencia@gmail.com>
2013-12-13 16:56:15 +0000

    install commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Alegrianocaos::Application.config.secret_key_base = '68ec10564129013f1a1cc13bc06a599f9842af7ccfa33f7bf1a86f2bc45d8462e3599535ffa60d7677cbb3f855b169f5f54d8a13b1789683dc3c00bc17328903'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Alegrianocaos::Application.config.session_store :cookie_store, key: '_alegrianocaos_session'
